bdb86036b61bf95c82b25d15a31c578299ee9308
Astear17 <go548394@gmail.com>
2026-05-21 20:26:40 +0700

    fix: update middleware to use withLocales for correct i18n routing

--- src/middleware.js
+++ src/middleware.js
@@ -1,2 +1,5 @@
-import { locales } from "nextra/locales"
-export const middleware = locales
+import { withLocales } from 'nextra/locales'
+
+export const middleware = withLocales((request) => {
+  // Empty middleware to just trigger Nextra's default i18n rewrite
+})
