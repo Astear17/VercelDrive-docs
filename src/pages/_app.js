5b8b750583312368239ce1d9e486c64962c8c08d
Astear17 <go548394@gmail.com>
2026-06-20 12:36:33 +0700

    fix: load Geist fonts via @font-face from public/, remove next/font/local

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,19 +2,6 @@ import '../../style.css'
 import '../style.css'
 import 'nextra-theme-docs/style.css'
 import Script from 'next/script'
-import localFont from 'next/font/local'
-
-const geistSans = localFont({
-  src: '../../public/fonts/geist-sans/Geist-Variable.woff2',
-  variable: '--font-geist-sans',
-  display: 'swap',
-})
-
-const geistMono = localFont({
-  src: '../../public/fonts/geist-mono/GeistMono-Variable.woff2',
-  variable: '--font-geist-mono',
-  display: 'swap',
-})
 
 export default function Nextra({ Component, pageProps }) {
   return (
@@ -31,9 +18,7 @@ export default function Nextra({ Component, pageProps }) {
           gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
         `}
       </Script>
-      <div className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   )
 }
